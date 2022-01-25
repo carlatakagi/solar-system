dc17c6dc58ce3b24427470df59f2c9267367dea4
carlatakagi <carlatakagi@gmail.com>
2022-01-25 17:38:10 -0400

    requisito 7, 8, 9

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Title from './Title';
+import missions from '../data/missions';
 
 class Missions extends React.Component {
   render() {
     return (
       <div data-testid="missions">
-        missions
+        <Title headline="Missões" />
       </div>
     );
   }
--- src/components/PlanetCard.js
+++ src/components/PlanetCard.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import '../data/planets';
 
 class PlanetCard extends React.Component {
   render() {

023190becdc9f58d2245039771af08982abd326c
carlatakagi <carlatakagi@gmail.com>
2022-01-25 15:16:19 -0400

    requisito 1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Header from './components/Header';
+import './App.css';
 
 class App extends React.Component {
   render() {
     return (
-      <p>Sistema Solar</p>
+      <div>
+        <Header />
+      </div>
     );
   }
 }

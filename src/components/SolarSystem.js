dab9df1cb58610aaf756986826630b4297a9457e
carlatakagi <carlatakagi@gmail.com>
2022-01-25 15:19:20 -0400

    requisito 2

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,1 +1,11 @@
 import React from 'react';
+
+class SolarSystem extends React.Component {
+  render() {
+    return (
+      <div data-testid="solar-system" />
+    );
+  }
+}
+
+export default SolarSystem;
